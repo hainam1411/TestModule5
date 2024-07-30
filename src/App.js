5fa4c7818f56ee70bf7d1cf8766d1dc26fc51c67
hainam1411 <hainam141199@gmail.com>
2024-07-30 10:58:42 +0700

    commit1

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import logo from './logo.svg';
+import './App.css';
+import {Route, Routes} from "react-router-dom";
+import React from "react";
+
+import OrderList from "./Components/Order/OrderList";
+import OrderAdd from "./Components/Order/OrderAdd";
+
+function App() {
+    return (
+
+        <>
+            <Routes>
+                <Route
+            </Routes>
+        </>
+    );
+}
+
+export default App;
